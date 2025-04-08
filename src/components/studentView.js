3bddc9593a52f39bc3c57e1219ddd12e1696950a
sann-man <rahsaanyj@gmail.com>
2025-04-08 11:42:40 -0700

    added view pages with routes

---
+++ src/components/studentView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="">
+      <h1>Student view</h1>
+    </div>
+  );
+}
+
+export default App;
